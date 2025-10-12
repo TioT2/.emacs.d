a45ab2042622de1f739a3d1e978850d60b47c5d2
TioT2 <piopi4112@gmail.com>
2025-10-12 05:59:37 +0300

    ORG, SLIME, CORFU packages added. Evil ':wq' and ubiquity fixed.

--- init.el
+++ init.el
@@ -6,7 +6,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(org-agenda-files nil)
+ '(package-selected-packages '(corfu evil-collection evil-commentary magit slime)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,13 +20,13 @@
   :config
 
   ;; Add package archives (org, melpa, melpa-stable)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org"          . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/") t)
   (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
   (package-initialize)
 
   ;; Prioritize stability over recency for packages
-  (setq package-archive-priorities
+  (setf package-archive-priorities
 	'(("gnu"          . 50)
 	  ("nongnu"       . 40)
 	  ("melpa-stable" . 30)
@@ -34,7 +35,7 @@
 
 
 ;; Make evil-collection happy
-(setq evil-want-keybinding nil)
+(setf evil-want-keybinding nil)
 
 ;; Install EVIL (Extensible VI Layer) package
 (use-package evil
@@ -48,23 +49,58 @@
   :config
   (evil-commentary-mode))
 
-;; Install package that enables EVIL-flavoured keymap in ORG, Magit, etc.
+;; Install package that enables EVIL-flavoured keymaps for Magit, Corfu, etc.
 (use-package evil-collection
   :ensure t
   :config
-  (evil-collection-init))
+  (evil-collection-init)
+
+  ;; Enable EVIL mode flavoured bindings for specific modes only
+  ;; (=> save traditional (**documented**) key bindings for everything else)
+  (setf evil-collection-mode-list
+	'(corfu)))
 
 ;; Install Magit
 (use-package magit
   :ensure t)
 
+;; Install OrgMode
+(use-package org
+  :ensure t)
+
+;; Install package for CommonLisp development (SLIME) and setup it
+(use-package slime
+  :ensure t
+  :config
+
+  ;; Enable SLIME extensions
+  (slime-setup '(slime-fancy slime-quicklisp slime-asdf slime-mrepl))
+
+  ;; Enable SLIME in lisp-mode
+  (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+  (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+
+  ;; Specify SBCL as a lisp compiler
+  (setf inferior-lisp-program "sbcl"))
+
+;; Install autocomplete
+(use-package corfu
+  :ensure t
+  :config
+  (global-corfu-mode)
+  (setf corfu-auto t))
+
 ;; Remap ':q' and ':wq' commands to not terminate EMACS instance
 (evil-ex-define-cmd "q" 'kill-current-buffer)
 (evil-ex-define-cmd "wq" (lambda ()
+			   (interactive)
 			   (save-buffer)
 			   (kill-current-buffer)))
 
 ;; Disable EMACS backup files
-(setq make-backup-files nil)
+(setf make-backup-files nil)
+
+;; Decrease font size
+(set-face-attribute 'default nil :height 80)
 
 ;;; init.el ends here
