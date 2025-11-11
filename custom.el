c8dd94453302c269625245a773835427f7b6480e
TioT2 <piopi4112@gmail.com>
2025-11-11 22:25:29 +0300

    EGLOT performance improve, codestyle fixed.

--- custom.el
+++ custom.el
@@ -6,21 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(corfu-auto t)
- '(corfu-auto-delay 0)
- '(corfu-auto-prefix 0)
- '(corfu-quit-no-match nil)
- '(custom-enabled-themes '(wombat))
- '(custom-file (concat user-emacs-directory "custom.el"))
- '(evil-collection-mode-list '(corfu dired))
- '(evil-want-keybinding nil)
- '(helm-completion-style 'helm-fuzzy)
- '(helm-move-to-line-cycle-in-source nil)
- '(make-backup-files nil)
- '(package-archive-priorities
-   '(("gnu" . 50) ("nongnu" . 40) ("melpa-stable" . 30) ("melpa" . 20)
-     ("org" . 10)))
- '(package-selected-packages nil))
+ '(custom-enabled-themes '(modus-vivendi-tinted)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
