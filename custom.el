b6e95c36fd5279cb570cbd44858e5e549f77b198
TioT2 <piopi4112@gmail.com>
2025-11-15 17:42:01 +0300

    Hotfix to make config work)

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tinted))
+ '(custom-enabled-themes '(modus-vivendi-tinted)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
